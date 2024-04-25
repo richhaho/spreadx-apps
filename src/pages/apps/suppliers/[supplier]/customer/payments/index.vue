6ec65970fc48e8a8fd97a5465269bba0d22679b5
avoroy <avoroy@biztechadvisors.com>
2024-04-25 20:22:34 -0300

    payment page redesign

--- src/pages/apps/suppliers/[supplier]/customer/payments/index.vue
+++ src/pages/apps/suppliers/[supplier]/customer/payments/index.vue
@@ -66,118 +66,75 @@ function toOrderDetail(order) {
         md="9"
         lg="9"
       >
-        <VRow>
-          <VCol
-            cols="12"
-            lg="4"
-            sm="6"
-            xs="12"      
-            relative
-            class="mb-5"
-          >
-            <div class="d-flex justify-space-between mt-3 ml-3 mr-3 mb-1">
-              <VImg src="https://eta.ironrocksoftware.com/images/temp/1.jpg"  />
-            </div>
-            <div class="d-flex justify-center mt-3 ml-3 mr-3 mb-1">
-              <h3 class="text-center">Thank you</h3>
-            </div>
-            <div class="d-flex ml-3 mr-3 mb-1">
-              <span class="text-center">We have received your order. We will send an update with tracking details when we ship your items.</span>
-            </div>
-            <div class="d-flex justify-center mt-5 ml-3 mr-3 mb-1">
-              <VBtn color="success" class="w-100" :to="`/apps/suppliers/${supplier_id}`">Back Home</VBtn>
-            </div>
-          </VCol>
-          <VCol
-            cols="12"
-            lg="4"
-            sm="6"
-            xs="12"
-            relative
-            class="mb-5"
-          >
-            <VCard class="ml-2 mr-2 pl-3 pr-3 pt-3 pb-3 mt-5" color="#def8e3">
-              <div class="d-flex justify-space-between align-center">
-                <div>
-                  <h5 class="ml-1">#{{ order.invoice_no }}</h5>
-                  <h4 class="ml-1">AED {{ Math.round(order.grand_total * 100)/100 }}</h4>
-                </div>
-                <VBtn color="success" size="small" @click="toOrderDetail(order)" :to="`/apps/suppliers/${supplier_id}/customer/orders/${order.reference_no}`">Details</VBtn>
-              </div>
-            </VCard>
-            <VCard class="ml-2 mr-2 pl-3 mt-5 pr-3 pt-3 pb-3" color="#f4f3f3">
-              <div class="d-flex justify-space-between align-center">
-                <div>
-                  <span class="ml-1 text-xs">Delivery Timeslot</span>
-                  <h6 class="ml-1">{{ order.delivery_date }}</h6>
-                </div>
-                <VBtn color="success" size="small">Details</VBtn>
-              </div>
-            </VCard>
-            <div class="d-flex justify-space-between mt-5 pt-5 ml-3 mr-3 mb-1">
-              <span class="text-sm">Your Items</span>
-              <h5>{{ order.item_count || 0 }}</h5>
-            </div>
-            <VDivider />
-            <div class="d-flex justify-space-between mt-5 pt-5 ml-3 mr-3 mb-1">
-              <span class="text-sm">Payment</span>
-              <h5>Pay with {{ order.invoice_type }}</h5>
-            </div>
-            <VDivider />
-            <div class="d-flex justify-space-between mt-5 pt-5 ml-3 mr-3 mb-1">
-              <span class="text-sm">Email</span>
-              <h5>{{ userData.email }}</h5>
-            </div>
-            <VDivider />
-            <div class="d-flex justify-space-between mt-5 pt-5 ml-3 mr-3 mb-1">
-              <span class="text-sm">Mobile</span>
-              <h5>{{ userData.phone || '+971505140401'}}</h5>
-            </div>
-          </VCol>
-          <VCol
-            cols="12"
-            lg="4"
-            sm="12"        
-            relative
-            class="mb-5"
-          >
-            <VCard class="mr-2 pl-3 pr-3 pt-3 pb-3 mt-5" color="#def8e3">
-              <div class="d-flex justify-space-between align-center">
-                <div>
-                  <h5>Refer & Earn</h5>
-                  <h5>Give AED 25, Get AED 25</h5>
-                  <span class="text-xs">Invite your friends to Barakat and you both can earn 250 FreshCoins</span>
-                </div>
-                <div>
-                </div>
-              </div>
-              <div class="d-flex justify-center align-center mt-5 pt-5">
-                  <h4>Your Referral Code:</h4>
-              </div>
-              <div class="d-flex justify-center align-center">
-                <div class="d-flex justify-space-between border-success-dash pl-2 pr-2 pt-2 pb-2">
-                  <h4>{{ order.reference_no }}</h4>
-                  <div class="ml-5">
-                    <VIcon size="30" color="success" icon="tabler-copy" @click="copyToClipboard(order.reference_no)"></VIcon>
-                    <span class="text-success">Copy</span>
-                  </div>
-                </div>
-              </div>
-
-              <div class="mt-5 pt-5">
-                <h4>How does it work?</h4>
-                <span class="text-xs">- Invite your friends to join Barakat and they get AED 25 off on their 1st oder + 250 FreshCoins (AED 25) on becoming VIP member.</span><br>
-                <span class="text-xs">- You also earn 250 FreshCoins (AED 25) once your friend complets 1st order.</span>
-              </div>
-
-            </VCard>
-          </VCol>
-        </VRow>
+      <VCard class="pr-5 pl-5">
+        <template v-slot:text >
+          <VRow style="display: flex; justify-content: space-between; ">
+            <VCol cols="4" class="d-flex">
+              <span class="mt-2">Show</span>
+              <v-select 
+              :items="['5', '25', '50', '75', '100', 'ALL']"
+              ></v-select>
+              
+              <span class="pt-2">entries</span>   
+              
+            </VCol>   
+            <VCol cols="5">
+              <span class="float-left mt-2">Search: </span>
+              <v-text-field class="w-75"
+                v-model="search"
+                label="Search"
+                prepend-inner-icon="mdi-magnify"
+                variant="outlined"
+                hide-details
+                single-line
+              ></v-text-field>
+            </VCol>
+          </VRow>
+        </template>
+        <v-data-table class="table" :headers="headers" :search="search" :item-value="item => `${item.num}-${item.version}`"  :items="desserts" items-per-page="5" show-select>
+        </v-data-table>
+      </VCard>
       </VCol>
     </VRow>
   </VCard>
 </template>
-
+<script>
+  export default {
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            title: '#',
+            align: 'start',
+            key: 'num',
+          },
+          {key:'invoice_date', title:'INVOICE DATE'},
+          {key:'invoice_number', title:'INVOICE NUMBER'},
+          {key:'invoice_total', title:'INVOICE TOTAL'},
+          {key:'balance',  title:'BALANCE'},
+          {key:'amount', title:'AMOUNT TO PAY'}
+        ],
+        desserts:[
+          {
+            num:'1',
+            invoice_date:'16-04-2024',
+            invoice_number:'IN25442',
+            invoice_total:'145.94',
+            balance : '145.95',
+          },
+          {
+            num:'2',
+            invoice_date:'16-04-2024',
+            invoice_number:'IN25443',
+            invoice_total:'9.45',
+            balance : '9.45',
+          }
+        ]
+      }
+    },
+  }
+</script>
 <style>
 .supplier-link {
   display: inline-block;
